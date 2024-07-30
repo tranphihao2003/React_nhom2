dfa9b7533d7f3eb7470c47ee9357802fad994940
kynam0305 <149651057+kynam0305@users.noreply.github.com>
2024-07-30 21:08:57 +0700

    bang_Stores
    bang_Stores

--- Assignment/src/views/stores/Stores.js
+++ Assignment/src/views/stores/Stores.js
@@ -1,28 +1,94 @@
-import React from 'react'
-import { CTable, CPagination, CPaginationItem } from '@coreui/react'
-import { CButton } from '@coreui/react'
+import React, { useEffect, useState, useRef } from 'react'
+import {
+  CButton,
+  CTable,
+  CPagination,
+  CPaginationItem,
+  CCard,
+  CCardHeader,
+  CRow,
+  CCol,
+} from '@coreui/react'
+import API_Store from '../../services/API/API_Store'
 import CIcon from '@coreui/icons-react'
 import * as icon from '@coreui/icons'
+import { useNavigate, useSearchParams, Link } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import ModalComponent from '../../components/modal/modalComponent'
+import AppHeaderHistory from '../../components/AppheaderHisory'
 
 const Stores = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [pagination, setPagination] = useState({
+    page: searchParams.get('page') ? parseInt(searchParams.get('page')) : 1,
+    pageSize: searchParams.get('pageSize') ? parseInt(searchParams.get('pageSize')) : 10,
+    totalItems: 0,
+    totalPages: 0,
+  })
+  const API_Class = new API_Store();
+  const [reloadheader, setreloadheader] = useState([])
+  const [items, setItems] = useState([])
+  const navigate = useNavigate()
+  useEffect(() => {
+    document.title = 'Cửa hàng'
+    getdata(pagination.page, pagination.pageSize)
+  }, [])
+  function getdata(page, pageSize) { // lấy data api
+    console.log(page, pageSize);
+    API_Class.getStore(page, pageSize).then((response) => {
+      setPagination({
+        totalItems: response.totalItems,
+        totalPages: response.totalPages,
+        page: response.currentPage,
+        pageSize: response.pageSize,
+      })
+      renderdata(response.stores)
+    })
+  }
+  function deleteacp(id) {
+    API_Class.changestatus(id, 1).then((response) => {
+      ShowSwal('success', 'Xóa thành công')
+      getdata(pagination.page, pagination.pageSize)
+      setreloadheader((reloadheader) => !reloadheader)
+    })
+  }
+  function editacp(id) {
+    navigate(`/Stores_edit/${id}`)
+  }
+  const ShowSwal = (status, title) => {
+    withReactContent(Swal).fire({
+      position: 'center',
+      icon: status,
+      title: title,
+      showConfirmButton: false,
+      timer: 1000,
+    })
+  }
+  function handlePageChange(newpage) {
+    console.log(newpage);
+    searchParams.set('page', newpage)
+    navigate(`/stores?${searchParams.toString()}`)
+    getdata(newpage, pagination.pageSize)
+  }
   const columns = [
     {
-      key: 'store_id',
+      key: 'STT',
       label: 'Mã cửa hàng',
       _props: { scope: 'col' },
     },
     {
-      key: 'store_name',
+      key: 'Store_Name',
       label: 'Tên cửa hàng',
       _props: { scope: 'col' },
     },
     {
-      key: 'store_location',
+      key: 'Store_Location',
       label: 'Địa chỉ',
       _props: { scope: 'col' },
     },
     {
-      key: 'store_phone',
+      key: 'Store_Phone',
       label: 'Số điện thoại',
       _props: { scope: 'col' },
     },
@@ -37,78 +103,100 @@ const Stores = () => {
       _props: { scope: 'col' },
     },
   ]
-
-  const items = [
-    {
-      store_id: 1,
-      store_name: 'Music World',
-      store_location: '123 Đường Âm nhạc, Hà Nội',
-      store_phone: '(024) 1234 5678',
-      status: <span className="badge bg-success">Còn hàng</span>,
-      actions: (
-        <>
-          <CButton variant="outline" color="danger">
-            <CIcon icon={icon.cilTrash} />
-          </CButton>{' '}
-          <CButton color="primary">
-            <CIcon icon={icon.cilPencil} />
-          </CButton>
-        </>
-      ),
-      _cellProps: { store_id: { scope: 'row' } },
-    },
-    {
-      store_id: 2,
-      store_name: 'Sound Palace',
-      store_location: '456 Đại lộ Âm thanh, TP. Hồ Chí Minh',
-      store_phone: '(028) 8765 4321',
-      status: <span className="badge bg-success">Còn hàng</span>,
-      actions: (
-        <>
-          <CButton variant="outline" color="danger">
-            <CIcon icon={icon.cilTrash} />
-          </CButton>{' '}
-          <CButton color="primary">
-            <CIcon icon={icon.cilPencil} />
-          </CButton>
-        </>
-      ),
-      _cellProps: { store_id: { scope: 'row' } },
-    },
-  ]
+  function renderdata(items) {
+    setItems(
+      items.map((item, index) => {
+        item.STT = index + 1
+        item.actions = (
+          <>
+            <ModalComponent
+              {...item}
+              color="danger"
+              content="bạn muốn xóa ?"
+              icon="cilTrash"
+              status="Delete"
+              actions={deleteacp}
+              id={item.Store_ID}
+              nameitems={item.Store_Name}
+            ></ModalComponent>{' '}
+            <ModalComponent
+              {...item}
+              color="primary"
+              content="bạn muốn chỉnh sửa?"
+              icon="cilPen"
+              status="Edit"
+              actions={editacp}
+              id={item.Store_ID}
+              nameitems={item.Store_Name}
+            ></ModalComponent>
+          </>
+        )
+        return item
+      }),
+    )
+  }
 
   return (
-    <div>
-      <CTable>
-        <thead>
-          <tr>
-            {columns.map((column) => (
-              <th key={column.key} {...column._props}>
-                {column.label}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) => (
-            <tr key={item.store_id}>
-              <td>{item.store_id}</td>
-              <td>{item.store_name}</td>
-              <td>{item.store_location}</td>
-              <td>{item.store_phone}</td>
-              <td>{item.status}</td>
-              <td>{item.actions}</td>
-            </tr>
-          ))}
-        </tbody>
-      </CTable>
-      <CPagination aria-label="Page navigation example">
-        <CPaginationItem>Trước</CPaginationItem>
-        <CPaginationItem>1</CPaginationItem>
-        <CPaginationItem>2</CPaginationItem>
-        <CPaginationItem>Tiếp</CPaginationItem>
+
+    <CCard>
+      <CCardHeader>
+        <CRow className="align-items-center">
+          <CCol sm="3">
+            <h5 id="traffic" className="card-title mb-0">
+              Danh sách nhà cung cấp
+            </h5>
+          </CCol>
+
+          <CCol sm="9" className="d-md-block">
+            <AppHeaderHistory
+              id="Store_ID"
+              API={API_Store}
+              path="Stores"
+              page={pagination.page}
+              loaddata={getdata}
+              status={reloadheader}
+            />
+            <CButton
+              onClick={() => navigate('/Stores_add')}
+              color="success"
+              className="float-end me-2 px-4 text-white"
+            >
+              <CIcon icon={icon.cilPlus} /> Thêm mới
+            </CButton>
+          </CCol>
+        </CRow>
+      </CCardHeader>
+      <div style={{ minHeight: '70vh' }}>
+        <CTable striped hover columns={columns} items={items} />
+
+        {items.lenght === 0 && <div className="text-center">khong co du lieu</div>}
+      </div>
+
+      <CPagination align="center" aria-label="Page navigation example">
+        <CPaginationItem
+          disabled={pagination.page === 1}
+          onClick={() => handlePageChange(pagination.page - 1)}
+        >
+          <span aria-hidden="true">&laquo;</span>
+        </CPaginationItem>
+        {Array.from({ length: pagination.totalPages }, (_, i) => i + 1).map((page) => (
+          <CPaginationItem
+            key={page}
+            active={page === pagination.page}
+            onClick={() => handlePageChange(page)}
+          >
+            {page}
+          </CPaginationItem>
+        ))}
+        <CPaginationItem
+          disabled={pagination.page === pagination.totalPages}
+          onClick={() => handlePageChange(pagination.page + 1)}
+        >
+          {' '}
+          <span aria-hidden="true">&raquo;</span>
+        </CPaginationItem>
       </CPagination>
-    </div>
+    </CCard>
   )
 }
 
--- Assignment/src/views/stores/Stores_Edit.js
+++ Assignment/src/views/stores/Stores_Edit.js
@@ -1,45 +1,154 @@
-import React from 'react'
-import { CCard, CCardBody, CForm, CFormInput, CButton, CFormLabel } from '@coreui/react'
+import React, { useEffect, useState } from 'react';
+import {
+  CButton,
+  CCol,
+  CForm,
+  CFormInput,
+  CFormLabel,
+  CAlert,
+  CCard,
+  CCardHeader,
+  CCardBody,
+} from '@coreui/react';
+import API_Store from '../../services/API/API_Store';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Stores_Edit = () => {
-  return (
-    <div>
-      <CCard>
-        <CCardBody>
-          <h2>Sửa Cửa Hàng</h2>
-          <CForm>
-            <CFormInput
-              type="text"
-              id="storeName"
-              label="Tên cửa hàng"
-              placeholder="Nhập tên cửa hàng"
-              style={{ marginBottom: '10px' }}
-            />
-            <CFormInput
-              type="text"
-              id="storeLocation"
-              label="Địa chỉ cửa hàng"
-              placeholder="Nhập địa chỉ cửa hàng"
-              style={{ marginBottom: '10px' }}
-            />
-            <CFormInput
-              type="text"
-              id="storePhone"
-              label="Số điện thoại"
-              placeholder="Nhập số điện thoại"
-              style={{ marginBottom: '10px' }}
-            />
-            <CButton type="submit" color="primary" style={{ marginRight: '10px' }}>
-              Lưu
-            </CButton>
-            <CButton type="button" color="danger">
-              Hủy
-            </CButton>
-          </CForm>
-        </CCardBody>
-      </CCard>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [status, setStatus] = useState(null);
+  const [validated, setValidated] = useState(false);
+  const [formData, setFormData] = useState({
+    Store_Name: '',
+    Store_Location: '',
+    Store_Phone: '',
+  });
 
-export default Stores_Edit
+  const API_Class = new API_Store();
+
+  useEffect(() => {
+    document.title = 'Sửa cửa hàng';
+
+    if (id) {
+      API_Class.getStorebyid(id)
+        .then((response) => {
+          console.log(response);
+          setFormData(response[0]);
+        })
+        .catch((error) => {
+          console.error('Error fetching store data:', error);
+          setStatus(false);
+        });
+    }
+  }, [id]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      updateStore();
+    }
+    setValidated(true);
+  };
+
+  const updateStore = () => {
+    API_Class.updateStore({ ...formData, Store_ID: id })
+      .then((response) => {
+        setStatus(true);
+        setTimeout(() => {
+          navigate('/Stores');
+        }, 200);
+      })
+      .catch((error) => {
+        console.error('Error updating store:', error);
+        setStatus(false);
+      });
+  };
+
+  return (
+    <CCard>
+      <CCardHeader>
+        <h3>Sửa cửa hàng</h3>
+      </CCardHeader>
+      <CCardBody>
+        <CForm className="row g-3" noValidate onSubmit={handleSubmit} validated={validated}>
+          {status === false && (
+            <CCol md={12}>
+              <CAlert color="danger">Sửa cửa hàng thất bại</CAlert>
+            </CCol>
+          )}
+          {status === true && (
+            <CCol md={12}>
+              <CAlert color="success">Sửa cửa hàng thành công</CAlert>
+            </CCol>
+          )}
+
+          <CCol md={6}>
+            <CFormLabel htmlFor="Store_Name">
+              <strong>Tên cửa hàng</strong>
+            </CFormLabel>
+            <CFormInput
+              type="text"
+              placeholder="Nhập tên cửa hàng"
+              autoComplete="Store_Name"
+              required
+              name="Store_Name"
+              value={formData.Store_Name}
+              onChange={handleChange}
+              feedbackInvalid="Vui lòng nhập tên cửa hàng"
+            />
+          </CCol>
+
+          <CCol md={6}>
+            <CFormLabel htmlFor="Store_Location">
+              <strong>Địa chỉ</strong>
+            </CFormLabel>
+            <CFormInput
+              required
+              type="text"
+              id="Store_Location"
+              name="Store_Location"
+              value={formData.Store_Location}
+              onChange={handleChange}
+              placeholder="Nhập địa chỉ"
+            />
+          </CCol>
+
+          <CCol md={6}>
+            <CFormLabel htmlFor="Store_Phone">
+              <strong>Số điện thoại</strong>
+            </CFormLabel>
+            <CFormInput
+              pattern="[0-9]{10}"
+              required
+              type="tel"
+              id="Store_Phone"
+              name="Store_Phone"
+              value={formData.Store_Phone}
+              onChange={handleChange}
+              feedbackInvalid="Vui lòng nhập số điện thoại"
+              placeholder="Nhập số điện thoại"
+            />
+          </CCol>
+          <CCol md={12}>
+            <CButton color="primary" type="submit">
+              Lưu thay đổi
+            </CButton>
+          </CCol>
+        </CForm>
+      </CCardBody>
+    </CCard>
+  );
+};
+
+export default Stores_Edit;
