79bd1277d7ffefbb9e7202848654ef462c545803
kynam0305 <149651057+kynam0305@users.noreply.github.com>
2024-07-30 21:08:57 +0700

    bang_Stores
    bang_Stores

--- Assignment/src/views/stores/Stores_Add.js
+++ Assignment/src/views/stores/Stores_Add.js
@@ -1,46 +1,140 @@
-import React from 'react'
-import { CCard, CCardBody, CButton, CForm, CFormInput, CFormLabel } from '@coreui/react'
+import React, { useEffect, useState } from 'react';
+import {
+  CButton,
+  CCol,
+  CForm,
+  CFormInput,
+  CFormLabel,
+  CAlert,
+  CCard,
+  CCardHeader,
+  CCardBody,
+} from '@coreui/react';
+// api
+import API_Store from '../../services/API/API_Store';
+// router
+import { useNavigate } from 'react-router-dom';
 
-const Stores_Add = () => {
+const Store_Add = () => {
+  const navigate = useNavigate(); // Sử dụng đúng tên hàm navigate
+  const [status, setStatus] = useState(null);
+  const [validated, setValidated] = useState(false);
+  const [count, setCount] = useState(0);
+  const API_Class = new API_Store();
+  const [formData, setFormData] = useState({
+    Store_Name: '',
+    Store_Location: '',
+    Store_Phone: '',
+  });
+
+  useEffect(() => {
+    document.title = 'Thêm cửa hàng';
+  }, [count]);
+
+  function create() {
+    API_Class.createStore(formData)
+      .then((response) => {
+        setStatus(true);
+        setTimeout(() => {
+          navigate('/Stores'); 
+        }, 700);
+      })
+      .catch((error) => {
+        setStatus(false);
+      });
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      create();
+    }
+    setValidated(true);
+    setCount(count + 1);
+  };
+
   return (
-    <div>
-      <CCard>
-        <CCardBody>
-          <h2>Thêm cửa hàng</h2>
-          <CForm style={{ margin: '10px' }}>
-            <CFormLabel htmlFor="storeName">Tên cửa hàng</CFormLabel>
+    <CCard>
+      <CCardHeader>
+        <h3>Thêm cửa hàng</h3>
+      </CCardHeader>
+      <CCardBody>
+        <CForm className="row g-3" noValidate onSubmit={handleSubmit} validated={validated}>
+          {status === false && (
+            <CCol md={12}>
+              <CAlert color="danger">Thêm cửa hàng thất bại</CAlert>
+            </CCol>
+          )}
+          {status === true && (
+            <CCol md={12}>
+              <CAlert color="success">Thêm cửa hàng thành công</CAlert>
+            </CCol>
+          )}
+
+          <CCol md={6}>
+            <CFormLabel htmlFor="Store_Name">
+              <strong>Tên cửa hàng</strong>
+            </CFormLabel>
             <CFormInput
-              style={{ marginBottom: '10px' }}
               type="text"
-              id="storeName"
               placeholder="Nhập tên cửa hàng"
+              autoComplete="Store_Name"
+              required
+              name="Store_Name"
+              onChange={handleChange}
+              feedbackInvalid="Vui lòng nhập tên cửa hàng"
             />
+          </CCol>
 
-            <CFormLabel htmlFor="storeLocation">Địa chỉ</CFormLabel>
+          <CCol md={6}>
+            <CFormLabel htmlFor="Store_Location">
+              <strong>Địa chỉ</strong>
+            </CFormLabel>
             <CFormInput
-              style={{ marginBottom: '10px' }}
+              required
               type="text"
-              id="storeLocation"
-              placeholder="Nhập địa chỉ cửa hàng"
+              id="Store_Location"
+              name="Store_Location"
+              onChange={handleChange}
+              placeholder="Nhập địa chỉ"
             />
+          </CCol>
 
-            <CFormLabel htmlFor="storePhone">Số điện thoại</CFormLabel>
+          <CCol md={6}>
+            <CFormLabel htmlFor="Store_Phone">
+              <strong>Số điện thoại</strong>
+            </CFormLabel>
             <CFormInput
-              style={{ marginBottom: '10px' }}
-              type="text"
-              id="storePhone"
+              pattern="[0-9]{10}"
+              required
+              type="tel"
+              id="Store_Phone"
+              name="Store_Phone"
+              onChange={handleChange}
+              feedbackInvalid="Vui lòng nhập số điện thoại"
               placeholder="Nhập số điện thoại"
             />
-
-            <CButton color="primary" style={{ marginRight: '10px' }}>
-              Thêm
+          </CCol>
+          <CCol md={12}>
+            <CButton color="primary" type="submit">
+              Thêm cửa hàng
             </CButton>
-            <CButton color="danger">Hủy</CButton>
-          </CForm>
-        </CCardBody>
-      </CCard>
-    </div>
-  )
-}
+          </CCol>
+        </CForm>
+      </CCardBody>
+    </CCard>
+  );
+};
 
-export default Stores_Add
+export default Store_Add;
